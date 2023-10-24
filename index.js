60204027b9c52bc9fa3e730cbed9f220e544f98d
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-24 19:01:10 +0530

    ExpressJs Setup,Response and Route

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+//console.dir(app);
+
+let port = 8080;
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}\n server initiated!!!`);
+})
+// app.use((req,res)=>{
+//     //console.log(req);
+    
+//     console.log("Request Received");
+//    // res.send("Response Received from Server!");
+//     // res.send({
+//     //     name: "CG",
+//     //     age: "20",
+//     // });
+//     res.send("<h1>Fruits</h1> <ul><li>Apple</li><li>Mango</li></ul>")
+// });
+app.get("/",(req,res)=>{
+    res.send("You Contacted ROOT Path");
+})
+app.get("/apple",(req,res)=>{
+    res.send("You Contacted APPLE Path");
+})
+app.get("/orange",(req,res)=>{
+    res.send("You Contacted orange Path");
+})
+app.get("*",(req,res)=>{
+    res.send("This path does not exist, please correct your path");
+});
+
+app.post("/",(req,res)=>{
+    res.send("You sent a post request to root path");
+})
