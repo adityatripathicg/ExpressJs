ea65761ac3998a0e3cc9d9fb2a6988c0bbe4636d
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-26 18:26:46 +0530

    Get & Post Req, Handling Post Req

---
+++ Miscellaneous/Backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.listen(port,()=>{
+    console.log("App Listening to port :",port);
+    console.log("Server Initiated");
+});
+app.get("/register",(req,res)=>{
+    let { user, password } = req.query;
+    res.send(`Standard GET Response, Welcome ${user}`);
+})
+app.post("/register",(req,res)=>{
+    console.log(req.body);
+    let { user, password } = req.body;
+    res.send(`Standard POST Response, Welcome ${user}`);
+})
